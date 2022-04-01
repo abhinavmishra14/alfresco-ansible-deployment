d853606b3dea276ac0bed73a30d033ff12ad8d24
Giovanni Toraldo <giovanni.toraldo@hyland.com>
2022-04-01 13:03:27 +0200

    more secure passwords

--- scripts/generate-secrets.sh
+++ scripts/generate-secrets.sh
@@ -2,7 +2,7 @@
 SECRET_KEYS=(repo_db_password sync_db_password reposearch_shared_secret)
 
 for SECRET_KEY in "${SECRET_KEYS[@]}"; do
-    RANDOM_STRING=$(openssl rand -base64 21)
+    RANDOM_STRING=$(openssl rand -base64 33)
     if [ -z "${ANSIBLE_VAULT_PASSWORD_FILE}" ] || [ "$1" == 'plaintext' ]; then
         echo "${SECRET_KEY}: \"$RANDOM_STRING\""
     else
